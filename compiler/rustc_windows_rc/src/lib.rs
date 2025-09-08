797c4b144e5d4141cb15cf30cb0f85e7eaf2b23a
Aleksey Kliger <alklig@microsoft.com>
2025-09-08 12:09:09 -0400

    use find-msvc-tools crate directly instead of cc

--- compiler/rustc_windows_rc/src/lib.rs
+++ compiler/rustc_windows_rc/src/lib.rs
@@ -2,9 +2,7 @@
 //!
 //! Uses values from the `CFG_VERSION` and `CFG_RELEASE` environment variables
 //! to set the product and file version information in the Windows resource file.
-use std::{env, ffi, fs, path, process};
-
-use cc::windows_registry;
+use std::{env, fs, path, process};
 
 /// The template for the Windows resource file.
 const RESOURCE_TEMPLATE: &str = include_str!("../rustc.rc.in");
@@ -35,8 +33,7 @@ pub fn compile_windows_resource_file(
     resources_dir.push("resources");
     fs::create_dir_all(&resources_dir).unwrap();
 
-    let resource_compiler =
-        find_resource_compiler(&env::var("CARGO_CFG_TARGET_ARCH").unwrap()).expect("found rc.exe");
+    let resource_compiler = find_resource_compiler().expect("found rc.exe");
 
     let rc_path = resources_dir.join(file_stem.with_extension("rc"));
 
@@ -134,25 +131,26 @@ fn parse_version(version: &str) -> Option<ResourceVersion> {
 
 /// Find the Windows SDK resource compiler `rc.exe` for the given architecture or target triple.
 /// Returns `None` if the tool could not be found.
-fn find_resource_compiler(arch_or_target: &str) -> Option<path::PathBuf> {
-    find_windows_sdk_tool(arch_or_target, "rc.exe")
+fn find_resource_compiler() -> Option<path::PathBuf> {
+    find_windows_sdk_tool("rc.exe")
 }
 
 /// Find a Windows SDK tool for the given architecture or target triple.
 /// Returns `None` if the tool could not be found.
-fn find_windows_sdk_tool(arch_or_target: &str, tool_name: &str) -> Option<path::PathBuf> {
-    // windows_registry::find_tool can only find MSVC tools, not Windows SDK tools, but
-    // cc does include the Windows SDK tools in the PATH environment of MSVC tools.
+fn find_windows_sdk_tool(tool_name: &str) -> Option<path::PathBuf> {
+    let (ucrt_dir, ucrt_version) = find_msvc_tools::get_ucrt_dir()?;
 
-    let msvc_linker = windows_registry::find_tool(arch_or_target, "link.exe")?;
-    let path = &msvc_linker.env().iter().find(|(k, _)| k == "PATH")?.1;
-    find_tool_in_path(tool_name, path)
-}
+    // Determine the host architecture to find the right subdirectory in the Windows SDK
+    let host = match env::var("HOST").ok()?.split('-').next()? {
+        "x86" => "x86",
+        "x86_64" => "x64",
+        "aarch64" => "arm64",
+        _ => return None,
+    };
 
-/// Find a tool in the directories in a given PATH-like string.
-fn find_tool_in_path<P: AsRef<ffi::OsStr>>(tool_name: &str, path: P) -> Option<path::PathBuf> {
-    env::split_paths(path.as_ref()).find_map(|p| {
-        let tool_path = p.join(tool_name);
-        if tool_path.try_exists().unwrap_or(false) { Some(tool_path) } else { None }
-    })
+    let mut tool_path = ucrt_dir.join("bin").join(&ucrt_version).join(host);
+
+    tool_path.push(tool_name);
+
+    Some(tool_path)
 }
